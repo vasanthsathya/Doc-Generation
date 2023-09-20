5cc40cad53d75baa434a1433943b555267e0587a
shubhangi-dell <shubhangi_srivastava@dell.com>
2023-09-20 13:01:22 +0530

    @123
    Signed-off-by: Shubhangi-dell <shubhangi_srivastava@dell.com>

--- telemetry/roles/omnia_telemetry_cp/files/regular_metric_collector.py
+++ telemetry/roles/omnia_telemetry_cp/files/regular_metric_collector.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#regular_metric_collector.py
-#!/usr/bin/env python3
+# regular_metric_collector.py
+# !/usr/bin/env python3
 '''
     Module to get all regular metrics
 '''
@@ -26,6 +26,7 @@ import utility
 import data_collector_slurm
 import prerequisite
 
+
 class RegularMetricCollector:
     '''
     RegularMetricCollector class is responsible for collecting all regular metrics.
@@ -33,6 +34,7 @@ class RegularMetricCollector:
 
     def __init__(self):
         self.regular_metric_output_dict = {}
+        self.regular_unit = {}
 
     def get_blocked_process(self):
         '''
@@ -47,10 +49,10 @@ class RegularMetricCollector:
             blocked_processes = tokens[1]
 
             # BlockedProcess is number of blocked processes waiting for I/O
-            self.regular_metric_output_dict["BlockedProcesses"] = blocked_processes + " processes"
+            self.regular_metric_output_dict["BlockedProcesses"] = blocked_processes
+            self.regular_unit["BlockedProcesses"] = "processes"
         else:
             self.regular_metric_output_dict["BlockedProcesses"] = utility.Result.NO_DATA.value
-
 
     def get_cpu_info(self):
         '''
@@ -59,30 +61,31 @@ class RegularMetricCollector:
         cputimes = data_collector_psutil.get_cpu_time_info()
         if cputimes is not None:
             # CPUSystem is CPU time spent in system mode
-            self.regular_metric_output_dict["CPUSystem"] = str(cputimes.system) + " seconds"
+            self.regular_metric_output_dict["CPUSystem"] = str(cputimes.system)
+            self.regular_unit["CPUSystem"] = "seconds"
             # CPUWait is CPU time spent in I/O wait mode
-            self.regular_metric_output_dict["CPUWait"] = str(cputimes.iowait) + " seconds"
+            self.regular_metric_output_dict["CPUWait"] = str(cputimes.iowait)
+            self.regular_unit["CPUWait"] = "seconds"
         else:
             self.regular_metric_output_dict["CPUSystem"] = utility.Result.NO_DATA.value
             self.regular_metric_output_dict["CPUWait"] = utility.Result.NO_DATA.value
-
 
     def get_packet_errors(self):
         '''
         Retrieve packet error information and store it in the dictionary.
         '''
         netio = data_collector_psutil.get_packet_info()
-        
+
         if netio is not None:
-            nmcli_output=invoke_commands.call_command("nmcli -t -f DEVICE,TYPE,STATE device status")
-            nmcli_data=common_parser.get_dict_list_format_parser_output(nmcli_output, ":", with_header=0)
+            nmcli_output = invoke_commands.call_command("nmcli -t -f DEVICE,TYPE,STATE device status")
+            nmcli_data = common_parser.get_dict_list_format_parser_output(nmcli_output, ":", with_header=0)
             # Initialize  interfaces list to store only ethernet connected devices
-            interface_list= []
+            interface_list = []
             # Loop through the nmcli_data and filter interfaces where header2 is ethernet and header3 is connected
             for i in range(len(nmcli_data['header1'])):
                 if nmcli_data['header2'][i] == 'ethernet' and nmcli_data['header3'][i] == 'connected':
                     interface_list.append(nmcli_data['header1'][i])
-        
+
             for interface in interface_list:
                 values = netio.get(interface)
                 if values is not None:
@@ -108,7 +111,8 @@ class RegularMetricCollector:
             hardware_corrupted = tokens[1]
 
             # Hardware corrupted memory detected by ECC
-            self.regular_metric_output_dict["HardwareCorruptedMemory"] = hardware_corrupted + " kB"
+            self.regular_metric_output_dict["HardwareCorruptedMemory"] = hardware_corrupted
+            self.regular_unit["HardwareCorruptedMemory"] = "kB"
         else:
             self.regular_metric_output_dict["HardwareCorruptedMemory"] = utility.Result.NO_DATA.value
 
@@ -119,23 +123,32 @@ class RegularMetricCollector:
         mem = data_collector_psutil.get_memory_info()
         if mem is not None:
             # MemoryFree is Free system memory
-            self.regular_metric_output_dict["MemoryFree"] = str(mem.free) + " bytes"
+            self.regular_metric_output_dict["MemoryFree"] = str(mem.free)
+            self.regular_unit["MemoryFree"] = "bytes"
             # MemoryTotal is Total memory on the system
-            self.regular_metric_output_dict["MemoryTotal"] = str(mem.total) + " bytes"
+            self.regular_metric_output_dict["MemoryTotal"] = str(mem.total)
+            self.regular_unit["MemoryTotal"] = "bytes"
             # MemoryAvailable is Available memory on the system
-            self.regular_metric_output_dict["MemoryAvailable"] = str(mem.available) + " bytes"
+            self.regular_metric_output_dict["MemoryAvailable"] = str(mem.available)
+            self.regular_unit["MemoryAvailable"] = "bytes"
             # MemoryPercent is the percentage usage as ((total - available) / total) * 100
             self.regular_metric_output_dict["MemoryPercent"] = str(mem.percent)
+            self.regular_unit["MemoryPercent"] = "percent"
             # MemoryUsed is memory used
-            self.regular_metric_output_dict["MemoryUsed"] = str(mem.used) + " bytes"
+            self.regular_metric_output_dict["MemoryUsed"] = str(mem.used)
+            self.regular_unit["MemoryUsed"] = "bytes"
             # MemoryActive is memory currently in use
-            self.regular_metric_output_dict["MemoryActive"] = str(mem.active) + " bytes"
+            self.regular_metric_output_dict["MemoryActive"] = str(mem.active)
+            self.regular_unit["MemoryActive"] = "bytes"
             # MemoryInactive is memory that is marked as not used
-            self.regular_metric_output_dict["MemoryInactive"] = str(mem.inactive) + " bytes"
+            self.regular_metric_output_dict["MemoryInactive"] = str(mem.inactive)
+            self.regular_unit["MemoryInactive"] = "bytes"
             # MemoryCached is cache for various things
-            self.regular_metric_output_dict["MemoryCached"] = str(mem.cached) + " bytes"
+            self.regular_metric_output_dict["MemoryCached"] = str(mem.cached)
+            self.regular_unit["MemoryCached"] = "bytes"
             # MemoryShared is memory that may be simultaneously accessed by multiple processes
-            self.regular_metric_output_dict["MemoryShared"] = str(mem.shared) + " bytes"
+            self.regular_metric_output_dict["MemoryShared"] = str(mem.shared)
+            self.regular_unit["MemoryShared"] = "bytes"
         else:
             self.regular_metric_output_dict["MemoryFree"] = utility.Result.NO_DATA.value
             self.regular_metric_output_dict["MemoryTotal"] = utility.Result.NO_DATA.value
@@ -151,34 +164,34 @@ class RegularMetricCollector:
         '''
         This method initiates slurm calls to data_collector_slurm and retrieves necessary values.
         '''
-        #sinfo
-        sinfo_dict=data_collector_slurm.get_cluster_values_sinfo()
-        self.regular_metric_output_dict["NodesTotal"]=sinfo_dict["NodesTotal"]
-        self.regular_metric_output_dict["NodesUp"]=sinfo_dict["NodesUp"]
-        self.regular_metric_output_dict["NodesDown"]=sinfo_dict["NodesDown"]
-        #squeue
-        squeue_dict=data_collector_slurm.get_cluster_values_squeue()
-        self.regular_metric_output_dict["QueuedJobs"]=squeue_dict["QueuedJobs"]
-        self.regular_metric_output_dict["RunningJobs"]=squeue_dict["RunningJobs"]
-        #sacct
-        sacct_dict=data_collector_slurm.get_cluster_values_sacct()
-        self.regular_metric_output_dict["FailedJobs"]=sacct_dict["FailedJobs"]
+        # sinfo
+        sinfo_dict = data_collector_slurm.get_cluster_values_sinfo()
+        self.regular_metric_output_dict["NodesTotal"] = sinfo_dict["NodesTotal"]
+        self.regular_metric_output_dict["NodesUp"] = sinfo_dict["NodesUp"]
+        self.regular_metric_output_dict["NodesDown"] = sinfo_dict["NodesDown"]
+        # squeue
+        squeue_dict = data_collector_slurm.get_cluster_values_squeue()
+        self.regular_metric_output_dict["QueuedJobs"] = squeue_dict["QueuedJobs"]
+        self.regular_metric_output_dict["RunningJobs"] = squeue_dict["RunningJobs"]
+        # sacct
+        sacct_dict = data_collector_slurm.get_cluster_values_sacct()
+        self.regular_metric_output_dict["FailedJobs"] = sacct_dict["FailedJobs"]
 
     def get_smart_regular_parameters(self):
         '''
         Get the following regular metric parameters:
         1. SMARTHDATemp
         '''
-        SMARTHDATemp_dict=data_collector_smart.get_using_smartctl("SMARTHDATemp")
+        SMARTHDATemp_dict = data_collector_smart.get_using_smartctl("SMARTHDATemp")
         for key in SMARTHDATemp_dict.keys():
-            self.regular_metric_output_dict["SMARTHDATemp:"+key]=SMARTHDATemp_dict[key]
+            self.regular_metric_output_dict["SMARTHDATemp:" + key] = SMARTHDATemp_dict[key]
 
     def get_unique_user_login(self):
         '''
         Get the following regular metric parameters:
         1. UniqueUserLogin
         '''
-        self.regular_metric_output_dict["UniqueUserLogin"]=str(data_collector_os.get_unique_loggedin_users())
+        self.regular_metric_output_dict["UniqueUserLogin"] = str(data_collector_os.get_unique_loggedin_users())
 
     def metric_collector(self, aggregation_level="compute"):
         '''
@@ -195,8 +208,8 @@ class RegularMetricCollector:
         else:
             self.regular_metric_output_dict["SMARTHDATemp"] = utility.Result.NO_DATA.value
 
-        #Get cluster level parameters.
-        if aggregation_level in ["manager","manager,login"]:
+        # Get cluster level parameters.
+        if aggregation_level in ["manager", "manager,login"]:
             if prerequisite.dict_component_existence["slurm"]:
                 # Get following informations through slurm
                 # 1.NodesDown
@@ -214,9 +227,7 @@ class RegularMetricCollector:
                 self.regular_metric_output_dict["RunningJobs"] = utility.Result.NO_DATA.value
                 self.regular_metric_output_dict["FailedJobs"] = utility.Result.NO_DATA.value
 
-
-
-        if aggregation_level in ["login","manager,login"]:
+        if aggregation_level in ["login", "manager,login"]:
             # Get the Folloiwng parameter
-            #1. UniqueUserLogin
+            # 1. UniqueUserLogin
             self.get_unique_user_login()
