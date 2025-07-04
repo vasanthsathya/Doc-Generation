a89cba3e9290d96018213d6391d0f894a2d9ae1d
Vrinda Marwah <vrinda.marwah@dell.com>
2025-07-04 17:17:59 +0530

    Update entrypoint.sh
    Signed-off-by: Vrinda Marwah <vrinda.marwah@dell.com>

--- ContainerFile/omnia_core/entrypoint.sh
+++ ContainerFile/omnia_core/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Call script to copy pulp cert
+/cert-copy.sh
+
 # Grab the hashed password
 omnia_core_hashed_passwd=$(grep omnia_core_hashed_passwd /opt/omnia/.data/oim_metadata.yml | awk -F: '{print $2}' | tr -d ' ')
 
